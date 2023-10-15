001daa469a09d4fea607222994ce391f69afc91a
Beladiri Buulolo <macpro@Macs-MacBook-Pro.local>
2023-10-15 14:06:57 +0700

    update tab foods

--- src/pages/Allfoods.js
+++ src/pages/Allfoods.js
@@ -1,75 +1,90 @@
-import React, { useState } from 'react';
-import { Typography, makeStyles, Button, Tabs, Tab, Box } from '@material-ui/core';
-import SwipeableViews from 'react-swipeable-views';
-import Homemenuitem from '../components/home/Homemenuitem';
-import menudata from '../utils/menudata';
+import React, { useEffect, useState } from "react";
+import {
+  Typography,
+  makeStyles,
+  Button,
+  Tabs,
+  Tab,
+  Box,
+} from "@material-ui/core";
+import SwipeableViews from "react-swipeable-views";
+import Homemenuitem from "../components/home/Homemenuitem";
+import menudata from "../utils/menudata";
 
 const useStyles = makeStyles((theme) => ({
   homemenu: {
-    paddingBottom: '50px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    '@media (max-width: 500px)': {
-      padding: '50px 0px',
+    paddingBottom: "50px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    "@media (max-width: 500px)": {
+      padding: "50px 0px",
     },
   },
   leaderBoard_left_h1: {
-    lineHeight: '40px',
-    fontFamily: 'Inter, sans-serif',
-    fontSize: '1.8rem',
-    fontWeight: 'bold',
-    marginTop: '17px',
-    textAlign: 'center',
-    marginBottom: '20px',
+    lineHeight: "40px",
+    fontFamily: "Inter, sans-serif",
+    fontSize: "1.8rem",
+    fontWeight: "bold",
+    marginTop: "17px",
+    textAlign: "center",
+    marginBottom: "20px",
   },
   homemenu_menu: {
-    fontFamily: 'Inter, sans-serif',
-    fontWeight: 'bold',
-    fontSize: '1rem',
+    fontFamily: "Inter, sans-serif",
+    fontWeight: "bold",
+    fontSize: "1rem",
   },
   homemenu_explore: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   homemenu_data: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginBottom: '30px',
-    flexWrap: 'wrap',
-    paddingTop: '30px',
-    '@media (max-width: 500px)': {
-      paddingTop: '0px',
+    display: "flex",
+    justifyContent: "center",
+    marginBottom: "30px",
+    flexWrap: "wrap",
+    paddingTop: "30px",
+    "@media (max-width: 500px)": {
+      paddingTop: "0px",
     },
   },
   button: {
-    borderRadius: '20px',
-    width: '150px',
-    marginTop: '10px',
-    textTransform: 'lowercase',
-    background: 'white',
-    border: '1px solid grey',
+    borderRadius: "20px",
+    width: "150px",
+    marginTop: "10px",
+    textTransform: "lowercase",
+    background: "white",
+    border: "1px solid grey",
   },
 }));
 
 export default function Allfoods() {
+  const { homemenu_menu, homemenu_explore, homemenu, homemenu_data } =
+    useStyles();
+  const [menus, setmenus] = useState([]);
+  const [tabs, settabs] = useState([]);
+  const [tabSelect, setTabSelect] = useState(0);
 
-  const {
-    homemenu_menu,
-    homemenu_explore,
-    homemenu,
-    homemenu_data,
-  } = useStyles();
-
-  const [tabSelect, setTabSelect] = useState(0)
+  useEffect(() => {
+    if (menudata) {
+      const newMenu = [...menudata];
+      let findCategory = [...new Set(newMenu.map((x) => x.category))];
+      let fic = newMenu.filter((x) => x.category === findCategory[tabSelect]);
+      setmenus(fic);
+      settabs(findCategory);
+    }
+  }, [menudata]);
 
   const handleChange = (event, newValue) => {
-    setTabSelect(newValue)
-  }
-  console.log('====================================');
-  console.log(typeof tabSelect);
-  console.log('====================================');
+    const newMenu = [...menudata];
+    const findTab = [...tabs];
+    let fic = newMenu.filter((x) => x.category === findTab[newValue]);
+    setmenus(fic);
+    setTabSelect(newValue);
+  };
+
   return (
     <div className={homemenu}>
       <div className={homemenu_explore}>
@@ -86,19 +101,16 @@ export default function Allfoods() {
         variant="fullWidth"
         aria-label="full width tabs example"
       >
-        <Tab label="Makanan" value={0} />
-        <Tab label="Minuman" value={1} />
+        {tabs.map((tab, index) => (
+          <Tab key={index} label={tab} value={index} />
+        ))}
       </Tabs>
-      <SwipeableViews
-        axis={'x'}
-        index={tabSelect}
-      // onChangeIndex={handleChangeIndex}
-      >
+      <SwipeableViews axis={"x"} index={tabSelect}>
         {tabSelect === 0 && (
           <Box p={3}>
             <Typography>
               <div className={homemenu_data}>
-                {menudata.map((data, index) => (
+                {menus.map((data, index) => (
                   <Homemenuitem key={index} {...data} />
                 ))}
               </div>
@@ -109,7 +121,7 @@ export default function Allfoods() {
           <Box p={3}>
             <Typography>
               <div className={homemenu_data}>
-                {menudata.map((data, index) => (
+                {menus.map((data, index) => (
                   <Homemenuitem key={index} {...data} />
                 ))}
               </div>
@@ -117,26 +129,6 @@ export default function Allfoods() {
           </Box>
         )}
       </SwipeableViews>
-      {/*
-      <div className={homemenu_data}>
-        {tabSelect === 0 && (
-          menudata.map((data, index) => (
-            <Homemenuitem key={index} {...data} />
-          ))
-        )}
-      </div>
-      <div className={homemenu_data}>
-        {tabSelect === 1 && (
-          menudata.map((data, index) => (
-            <Homemenuitem key={index} {...data} />
-          ))
-        )}
-      </div> */}
-      {/* <div className={homemenu_data}>
-        {menudata.map((data, index) => (
-          <Homemenuitem key={index} {...data} />
-        ))}
-      </div> */}
     </div>
   );
 }
